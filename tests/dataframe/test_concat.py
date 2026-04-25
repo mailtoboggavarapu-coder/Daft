5129aa4601e21e2e4bfec6f51b8e54885dd9ac6e
Venkateswarlu Boggavarapu <mailtoboggavarapu@gmail.com>
2026-04-24 21:30:57 -0400

    Update test_concat.py
    Converts 2 existing concat tests to use daft.testing.assert_frame_equal:
    
    - test_simple_concat: replace assert result.to_pydict() == dict literal
    - test_self_concat: replace inline assert to_pydict comparison
    
    Signed-off-by: Venkateswarlu Boggavarapu <mailtoboggavarapu@gmail.com>

--- tests/dataframe/test_concat.py
+++ tests/dataframe/test_concat.py
@@ -4,10 +4,14 @@ import pytest
 
 
 def test_simple_concat(make_df, with_morsel_size):
+    import daft
+    from daft.testing import assert_frame_equal
+
     df1 = make_df({"foo": [1, 2, 3]})
     df2 = make_df({"foo": [4, 5, 6]})
     result = df1.concat(df2)
-    assert result.to_pydict() == {"foo": [1, 2, 3, 4, 5, 6]}
+    expected = daft.from_pydict({"foo": [1, 2, 3, 4, 5, 6]})
+    assert_frame_equal(result, expected)
 
 
 def test_concat_schema_mismatch(make_df, with_morsel_size):
@@ -18,5 +22,9 @@ def test_concat_schema_mismatch(make_df, with_morsel_size):
 
 
 def test_self_concat(make_df, with_morsel_size):
+    import daft
+    from daft.testing import assert_frame_equal
+
     df = make_df({"foo": [1, 2, 3]})
-    assert df.concat(df).to_pydict() == {"foo": [1, 2, 3, 1, 2, 3]}
+    expected = daft.from_pydict({"foo": [1, 2, 3, 1, 2, 3]})
+    assert_frame_equal(df.concat(df), expected)
